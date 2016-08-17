06efce06aa79f2eb1a90804144d365bf326af995
Alison Stuart <alisonnicolestuart@gmail.com>
2016-08-16 22:30:32 -0400

    Here's the first draft of my greybox of the Tennis Quiz app

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict;'
+
+/* THIS PAGE IS JUST A GENERAL OUTLINE OF SOME OF THE COMPONENTS I WILL NEED WHEN I MAKE MY JS FILE
+
+
+//ready function opens here
+var question {
+    id: unassigned,
+        content: //whatever's in the div
+        answered: 0,
+
+}
+
+        //jquery handler for starting the quiz
+        startQuiz.click--> takeQuiz()
+
+        takeQuiz: function(){
+            quizDiv.show();
+            for (i = 1, i<=10, i++){
+                this.id = ("q"+i);
+            }
+
+            nextQuestion: function(){
+                if ('this.answered == 0'){
+                    //SHOW NEXT QUESTION
+                }
+            }
+
+            showResults: function(){
+                document.write("Here are your results: you scored ____/10, or ____%, on this quiz.")
+            }
+
+            showResults();
+        }//close ready function
+
+        */
+/
